aa3bc3fee05bfb53dc87588c2fe71473b20d2bd6
Sumerin <sumekpl@gmail.com>
2018-05-13 16:13:39 +0200

    Data/Interface

--- Proj2/DataBaseWcfData/DisplayData/Produkt.cs
+++ Proj2/DataBaseWcfData/DisplayData/Produkt.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataTemplates.DisplayData
 {
+    [DataContract]
     public class Produkt
     {
         [Key]
+        [DataMember]
         public int ID { get; set; }
 
         [StringLength(255)]
+        [DataMember]
         public string Nazwa { get; set; }
-        
+
+        [DataMember]
         public decimal? Cena { get; set; }
-        
+
+        [DataMember]
         public int? Dostepnosc { get; set; }
     }
 }
